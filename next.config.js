101f2425e5029f838cacfc2b92a98a18ff01b588
astroboy20 <tolulopeakinkunmi7@gmail.com>
2023-03-11 17:15:31 +0100

    registering tmdb in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['https://image.tmdb.org/', 'https://api.themoviedb.org/','https://api.themoviedb.org/3'],
+    domains: ['https://image.tmdb.org/', 'https://api.themoviedb.org/','https://api.themoviedb.org/3',"image.tmdb.org",'tmdb.org', 'themoviedb.org','localhost',"image.tmdb.org" ],
   },
 }
 
